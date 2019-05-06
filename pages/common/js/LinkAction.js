0c809f16e78c5e9e1f0c470a906343d3a7848f00
pablo <peybol9858@gmail.com>
2019-05-07 00:12:22 +0200

    redirect via js

---
+++ pages/common/js/LinkAction.js
@@ -0,0 +1,31 @@
+var workOnLocal = false;
+var remote = {
+    home: "http://pmartinezm.github.io",
+    regularidad: "pages/regularidad/index.html"
+};
+
+var local = {
+    home: "./index.html",
+    regularidad: "./pages/regularidad/index.html"
+};
+
+function redirect(pageKey) {
+    var uri;
+    var pages = workOnLocal ? local : remote;
+
+    if (workOnLocal) {
+        uri = pages[pageKey];
+    } else {
+        if (pageKey == "home") {
+            uri = pages["home"];
+        } else {
+            uri = pages["home"] + "/" + pages[pageKey];
+        }
+    }
+
+    window.location.replace(uri);
+    console.log("Working on local: " + workOnLocal);
+    console.log("Key: " + pageKey);
+    console.log("Pages: ", pages);
+    console.log("Redirect: " + uri);
+}
