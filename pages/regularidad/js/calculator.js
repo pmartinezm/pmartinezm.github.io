89b32ac485b09f3d4598b2f5c1b3792d783b94a0
Pablo Martínez <47028613+pmartinezm@users.noreply.github.com>
2019-05-07 14:12:11 +0200

    Update calculator.js

--- pages/regularidad/js/calculator.js
+++ pages/regularidad/js/calculator.js
@@ -1,13 +1,23 @@
-var distance, wheel, freq, vel, time;
+var distance, wheel, freq, vel, time, cont;
 
 function calculateTime() {
   getData();
+  
+  var res = "RESULTADOS\n" + 
+      "  Tiempo: " + distance / vel + "h // " + (distance / vel) * 60 * 60 + "s\n";
+  cont.innerText += res;
+  
   console.log("RESULTADOS");
   console.log("  Tiempo: " + distance / vel + "h // " + (distance / vel) * 60 * 60 + "s");
 }
 
 function calculateSpeed() {
   getData();
+  
+  var res = "RESULTADOS\n" + 
+      "  Velocidad media: " + distance / time + " km/h\n";
+  cont.innerText += res;
+  
   console.log("RESULTADOS");
   console.log("  Velocidad media: " + distance / time + "km/h");
 }
@@ -18,6 +28,16 @@ function getData() {
   freq = parseFloat($("freq").value);
   vel = parseFloat($("vel").value);
   time =parseFloat($("time").value);
+  
+  cont = document.getElementsByClassName("results-results");
+  
+  var res = "DATOS INTRODUCIDOS\n" + 
+      "  Distancia: " + distance + "\n" + 
+      "  Longitud rueda: " + wheel + "\n" + 
+      "  Vueltas/sec: " + freq + "\n" + 
+      "  Velocidad: " + vel + "\n" + 
+      "  Tiempo invertido: " + time + "\n";
+  cont.innerText = res;
   
   console.log("DATOS INTRODUCIDOS");
   console.log("  Distancia: " + distance);
