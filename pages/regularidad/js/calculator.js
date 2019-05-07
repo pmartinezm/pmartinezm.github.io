e44bd790134aec64ef9ddb0c30ae87980286771c
Pablo Martínez <47028613+pmartinezm@users.noreply.github.com>
2019-05-07 13:38:12 +0200

    Update calculator.js

--- pages/regularidad/js/calculator.js
+++ pages/regularidad/js/calculator.js
@@ -1,10 +1,17 @@
 var distance, wheel, freq, vel, time;
 
 function calculate() {
+  getData();
+  
+  console.log("Tiempo: " + distance / vel);
 }
 
 function getData() {
-  $("distance").
+  distance = parseInt($("distance").value);
+  wheel = parseInt($("wheel"));
+  freq = parseInt($("freq").value);
+  vel = parseInt($("vel").value);
+  time =parseInt($("time").value);
 }
 
 function $(id) {
