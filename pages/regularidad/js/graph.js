7ef11bcea56833ea671fc6c356b54f6407675011
Pablo Martínez <47028613+pmartinezm@users.noreply.github.com>
2019-05-07 16:05:04 +0200

    Update graph.js

--- pages/regularidad/js/graph.js
+++ pages/regularidad/js/graph.js
@@ -19,9 +19,16 @@ function simulate() {
   for(var i = 0; i < readings.length; i++) {
     var x = canvas.width / count;
     ctx.fillStyle = "#ffc321";
-    ctx.fillRect(x*i, canvas.height - 50 - readings[i], 10, 10);
+    drawDot(x*i, canvas.height - 50 - readings[i], 10);
+    //ctx.fillRect(x*i, canvas.height - 50 - readings[i], 10, 10);
   }
-};
+}
+
+function drawDot(x, y, size) {
+  var x = x + size / 2;
+  var y = y - size / 2;
+  ctx.fillRect(x, y, size, size);
+}
 
 function generateSpeed(max, min) {
   return Math.random()*max+min;
