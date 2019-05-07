42770258f3730f65d7b76e6dfbf51c4357a8ba99
Pablo Martínez <47028613+pmartinezm@users.noreply.github.com>
2019-05-07 16:49:16 +0200

    Update speedProvider.js

--- pages/regularidad/js/speedProvider.js
+++ pages/regularidad/js/speedProvider.js
@@ -1,1 +1,15 @@
+function generateSpeed(readings) {
+  readings = readings || 10;
+  var data = [];
+  
+  for(var i = 0; i < readings; i++) {
+    data.push(generateSpeed(50,40));
+  }
+  data[0] = 0;
+  
+  return data;
+}
 
+function generateSpeed(max, min) {
+  return Math.random()*max+min;
+}
